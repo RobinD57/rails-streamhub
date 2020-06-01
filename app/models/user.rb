6a892a803fd99b933f0f413978e4ed6eb808fdde
RobinD57 <robin.davidseth@gmail.com>
2020-06-01 15:26:50 +0200

    started integration of oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i(twitch mixer)
   has_many :identities
   validates :username, presence: true
+
+  def self.create_with_omniauth(info)
+    create(name: info['name'])
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,8 +259,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :twitch, ENV.fetch(TWITCH_APP_ID), ENV.fetch(TWITCH_APP_SECRET)
-  config.omniauth :mixer, ENV.fetch(MIXER_APP_ID), ENV.fetch(MIXER_APP_SECRET)
+  config.omniauth :twitch#, ENV.fetch(TWITCH_APP_ID), ENV.fetch(TWITCH_APP_SECRET)
+  config.omniauth :mixer#, ENV.fetch(MIXER_APP_ID), ENV.fetch(MIXER_APP_SECRET)
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
