c98d8be2c670bec145bc6eb5207285a9c7b2c285
RobinD57 <robin.davidseth@gmail.com>
2020-06-02 08:27:55 +0200

    added functionality to add multiple identities

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,8 +1,14 @@
 class Identity < ApplicationRecord
   belongs_to :user
   has_many :follows
+  validates_presence_of :uid, :provider
+  validates_uniqueness_of :uid, :scope => :provider
 
-  def self.create_from_provider_data(auth, user)
-    where(provider: auth["provider"], uid: auth["uid"], user: user).first_or_create
+  def self.find_for_oauth(auth)
+    find_or_create_by(uid: auth.uid, provider: auth.provider)
   end
 end
+
+#   def self.create_from_provider_data(auth, user)
+#    where(provider: auth["provider"], uid: auth["uid"], user: user).first_or_create
+#  end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } do
     resources :identities, shallow: true
   end
-  devise_for :identities
+  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
