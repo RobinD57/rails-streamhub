db6153bca6dc9cb6d3ff5fdd6c90879b67f873ac
Adam Sebesta <adam.sebesta5@gmail.com>
2020-06-02 11:32:24 +0200

    gen mock up follows controller for testing

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match '/logout', to: 'sessions#destroy', via: [:get, :post]
+  resources :follows, only: [:index, :create,:destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
