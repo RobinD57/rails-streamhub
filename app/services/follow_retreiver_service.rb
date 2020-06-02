f4bb529ea3f9b5b1c62b890f5a3ab2fa63769702
RobinD57 <robin.davidseth@gmail.com>
2020-06-02 18:40:33 +0200

    added the mixer API call handling for retreiving all followers of a user

--- app/services/follow_retreiver_service.rb
+++ app/services/follow_retreiver_service.rb
@@ -33,7 +33,7 @@ class FollowRetreiverService
   def twitch
     url = "https://api.twitch.tv/helix/users/follows?from_id=#{@identity.uid}"
     followers_serialized = open(url,
-      "Client-ID" => ENV["TWITCH_APP_ID"]
+      "Client-ID" => ENV["TWITCH_APP_ID"],
       "Authorization" => "Bearer #{@identity.token}").read
     followers = JSON.parse(followers_serialized)
 
@@ -42,10 +42,12 @@ class FollowRetreiverService
   end
 
   def mixer
-
+    url = "https://mixer.com/api/v1/users/#{@identity.uid}/follows"
+    followers_serialized = open(url).read # Mixer does not require authorization for this specific API call
+    followers = JSON.parse(followers_serialized)
   end
 end
 
 
-peter = Identity.new(uid: "32677136", token: "mhvpzha9o40uno204d0khw2vbnnq28", provider: "twitch")
+peter = Identity.new(uid: "180307681", token: "mhvpzha9o40uno204d0khw2vbnnq28", provider: "mixer")
 test = FollowRetreiverService.new(identity: peter).perform
