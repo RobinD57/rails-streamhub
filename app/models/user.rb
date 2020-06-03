bd2d59730c6da0a1e1f3527e9063b9bdd78b7a49
RobinD57 <robin.davidseth@gmail.com>
2020-06-03 14:59:11 +0200

    finished integration of Mixer service object and started with twitch

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,4 +51,12 @@ class User < ApplicationRecord
   def email_verified?
     self.email && self.email !~ TEMP_EMAIL_REGEX
   end
+
+  def get_follows
+    idents = current_user.identities
+    data = idents.each { |ident| FollowRetreiverService.new(identity: ident).perform }
+    # use that data for creating follows
+  end
+  # get follows => iterate => load follow for each identity => save them in follows db table
+  # load follows
 end
