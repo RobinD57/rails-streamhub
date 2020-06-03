18422a25b9c1ea072915d26bea3f2c8b3d50ddda
RobinD57 <robin.davidseth@gmail.com>
2020-06-03 16:09:23 +0200

    Twitch API handling service object done, but not properly tested yet

--- app/services/twitch_transform_service.rb
+++ app/services/twitch_transform_service.rb
@@ -6,19 +6,29 @@ class TwitchTransformService
 
 
   def perform
+    is_online?
     @twitch_follow_details.each { |follow| build_result_hash(follow) }
   end
 
   private
 
   def build_result_hash(follow)
-    result_hash = {
-      game_name: follow["type"]["name"],
-      stream_current_viewers: follow["type"]["viewersCurrent"],
-      stream_display_name: follow["name"],
-      stream_name: follow["token"],
-      stream_thumbnail: follow["bannerUrl"],
-      online_status: follow["online"]
-    }
+      result_hash = {
+        game_name: follow["type"]["name"],
+        stream_current_viewers: follow["type"]["viewersCurrent"],
+        stream_display_name: follow["name"],
+        stream_name: follow["token"],
+        stream_thumbnail: follow["bannerUrl"],
+        online_status: follow["online"]
+      }
+    end
+  end
+
+  def is_online?
+    @twitch_follow_details["data"].each do |follow|
+      if follow["type"] == "live"
+        build_result_hash(follow)
+      end
+    end
   end
 end
