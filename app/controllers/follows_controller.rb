354356b10ef3b20666324af1fa7ade6409f19597
RobinD57 <robin.davidseth@gmail.com>
2020-06-05 14:54:28 +0200

    fixed typo

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,7 +2,7 @@ class FollowsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    current_user.identities.map { |x| x.follows.destroy_all } # need to increase performance!
+    current_user.identities.map { |identity| identity.follows.destroy_all } # need to increase performance!
     @follows = current_user.get_follows
     # load follows from user model
     # check when they've been loaded, if not long ago just use cache
