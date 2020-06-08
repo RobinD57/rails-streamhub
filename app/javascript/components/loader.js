e3a60dcfdf59ad583fad7a819bd826597d51f452
Ami Onodera <62409630+ami-onodera@users.noreply.github.com>
2020-06-08 12:11:45 +0200

    Revert "loader working"

--- app/javascript/components/loader.js
+++
@@ -1,7 +0,0 @@
-const checkLoad = () => {
-  document.addEventListener('DOMContentLoaded', (event) => {
-      document.querySelector(".loader-wrapper").innerHTML = " "
-  });
-}
-
-export { checkLoad };
