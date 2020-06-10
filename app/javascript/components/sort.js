360d5458b7f454ab1c9e51b605cf4dd2d64f7d7d
RobinD57 <robin.davidseth@gmail.com>
2020-06-10 14:33:20 +0200

    added an event listener to the modal 'x' to remove the query params in the url and added guard clauses to existing functions

--- app/javascript/components/sort.js
+++ app/javascript/components/sort.js
@@ -1,32 +1,40 @@
 const fetchFollowsIndexCardsSortedByAlpha = () => {
-  document.querySelector(".alpha-filter").addEventListener("click",() => {
-     fetch(`/dashboard/sorted_collection?sort=alpha`, {
-    method: 'GET',
-    }).then(response => response.text())
-      .then((html) => {
-        document.querySelector(".reloading-area").innerHTML = html;
+  const alpha_filter = document.querySelector(".alpha-filter")
+  if (alpha_filter) {
+    alpha_filter.addEventListener("click",() => {
+       fetch(`/dashboard/sorted_collection?sort=alpha`, {
+      method: 'GET',
+      }).then(response => response.text())
+        .then((html) => {
+          document.querySelector(".reloading-area").innerHTML = html;
+      });
     });
-  });
+  };
 }
 
 const fetchFollowsIndexCardsSortedByViewers = () => {
-  document.querySelector(".viewers-filter").addEventListener("click",() => {
-    fetch(`/dashboard/sorted_collection?sort=views`, {
-    method: 'GET',
-    }).then(response => response.text())
-      .then((html) => {
-        document.querySelector(".reloading-area").innerHTML = html;
+  const viewers_filter = document.querySelector(".viewers-filter")
+  if (viewers_filter) {
+    viewers_filter.addEventListener("click",() => {
+      fetch(`/dashboard/sorted_collection?sort=views`, {
+      method: 'GET',
+      }).then(response => response.text())
+        .then((html) => {
+          document.querySelector(".reloading-area").innerHTML = html;
+      });
     });
-  });
+  };
 }
 
 const underlineSort = () => {
  const elements = document.querySelectorAll(".sort-btn")
-  elements.forEach((btn) => btn.addEventListener("click",(e) => {
-    elements.forEach((ele) => ele.classList.remove("underline"));
-    e.currentTarget.classList.toggle("underline");
-  })
-)};
+ if (elements) {
+    elements.forEach((btn) => btn.addEventListener("click",(e) => {
+      elements.forEach((ele) => ele.classList.remove("underline"));
+      e.currentTarget.classList.toggle("underline");
+    }));
+  };
+}
 
 
 export { fetchFollowsIndexCardsSortedByAlpha, fetchFollowsIndexCardsSortedByViewers,
