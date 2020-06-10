f5cc5b85928dfe6dd5f7892d5764ecf41ae6b202
Adam Sebesta <adam.sebesta5@gmail.com>
2020-06-10 17:58:14 +0200

    working on navbar scrolling

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -8,31 +8,34 @@ $( ".navbar-toggler").on( "click", function() {
 };
 
 const scrollShrinkFunction = () => {
-  if (nav)
+  if (nav) {
     document.addEventListener("scroll",(e) => {
-      if (window.scrollY > 10) {
+      if (window.scrollY > 60 && !nav.classList.contains("shrink")) {
         nav.classList.add("shrink");
         logo.style.display = "none";
-        console.log(e.scrollY)
-      }
+        console.log(window.scrollY);
+      };
     });
+  };
 };
 
 const scrollGrowFunction = () => {
-  if (nav)
+  if (nav.classList.contains("shrink")) {
     document.addEventListener("scroll",(e) => {
-      if (window.scrollY < 120) {
+      if (window.scrollY < 120 && window.scrollY > 60) {
         nav.classList.remove("shrink");
         logo.style.display = "";
-      }
+        console.log(window.scrollY);
+      };
     });
+  };
 };
 
 
 const scrollFinal = () => {
   scrollShrinkFunction();
   scrollGrowFunction();
-}
+};
 
 
 const closeNav = () => {
