6d11b2baafa678ab932a2fb91cf148c2f63b5f67
RobinD57 <robin.davidseth@gmail.com>
2020-06-12 10:57:04 +0200

    mixer embedding works with hard-coded streamer

--- app/javascript/components/embedded_stream.js
+++ app/javascript/components/embedded_stream.js
@@ -1,31 +1,23 @@
-let timer;
-let defaultCard;
 let frame;
 let iframe;
 
 const initEmbeddedStream = () => {
   const cards = document.querySelectorAll(".main-card");
   if (cards) {
-    cards.forEach((card) => card.addEventListener("mouseover", (event) => {
-      defaultCard = card.innerHTML;
-      const embedWrapper = card.querySelector(".embedWrapper");
-      embedWrapper.innerHTML = '<iframe style="display: none;" title="PaladinsGame\'s player frame" i18n-title="channel#ShareDialog:playerEmbedFrame|Embed player Frame copied from share dialog" allowfullscreen="true" src="https://mixer.com/embed/player/PaladinsGame?disableLowLatency=1" width="400" height="250"> </iframe>'
+    cards.forEach((card) => card.addEventListener("mouseenter", (event) => {
+      /* embedWrapper.innerHTML = '<iframe title="PaladinsGame\'s player frame" i18n-title="channel#ShareDialog:playerEmbedFrame|Embed player Frame copied from share dialog" allowfullscreen="true" src="https://mixer.com/embed/player/PaladinsGame?disableLowLatency=1" width="400" height="250"> </iframe>' */
       timer = setTimeout(function() {
-        iframe = embedWrapper.querySelector("iframe")
-        iframe.style.display = "block"
-      }, 8000);
-    }) /*.addEventListener("mouseleave", (event) => {
-        clearTimeout(timer);
-        iframe.style.display = "none";
-                setTimeout(function() {
-          card.classList.remove("hidden")
-        }, 1000)
-
-    }) */
-    );
+        const embedWrapper = card.querySelector(".embedWrapper");
+        const iframe = embedWrapper.querySelector("iframe");
+        iframe.src = "https://mixer.com/embed/player/ChefSsef?disableLowLatency=1"
+        embedWrapper.style.display = "block";
+      }, 2000);
+    }));
     cards.forEach((card) => card.addEventListener("mouseleave", (event) => {
-      clearTimeout(timer);
-      card.style.display = defaultCard;
+      const embedWrapper = card.querySelector(".embedWrapper");
+      const iframe = embedWrapper.querySelector("iframe")
+      iframe.src = ""
+      embedWrapper.style.display = "none";
     }));
   }
 }
