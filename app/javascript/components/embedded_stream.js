b531c5616dafcc29eac2ff92d05a721fab3667e4
RobinD57 <robin.davidseth@gmail.com>
2020-06-14 16:09:51 +0200

    added dataset attributes for dynamic generation of embed url. Mixer fully works, Twitch just needs a fix for the parent URL to support both streamhub and localhost

--- app/javascript/components/embedded_stream.js
+++ app/javascript/components/embedded_stream.js
@@ -5,18 +5,21 @@ const initEmbeddedStream = () => {
   const cards = document.querySelectorAll(".main-card");
   if (cards) {
     cards.forEach((card) => card.addEventListener("mouseenter", (event) => {
-      /* embedWrapper.innerHTML = '<iframe title="PaladinsGame\'s player frame" i18n-title="channel#ShareDialog:playerEmbedFrame|Embed player Frame copied from share dialog" allowfullscreen="true" src="https://mixer.com/embed/player/PaladinsGame?disableLowLatency=1" width="400" height="250"> </iframe>' */
-      timer = setTimeout(function() {
+      setTimeout(function() {
         const embedWrapper = card.querySelector(".embedWrapper");
         const iframe = embedWrapper.querySelector("iframe");
-        iframe.src = "https://mixer.com/embed/player/ChefSsef?disableLowLatency=1"
+        if (embedWrapper.dataset.source === "mixer") {
+          iframe.src = `https://mixer.com/embed/player/${embedWrapper.dataset.streamer}?disableLowLatency=1`;
+        } else if (embedWrapper.dataset.source === "twitch") {
+          iframe.src = `https://player.twitch.tv/?channel=${embedWrapper.dataset.streamer}&muted=true&autoplay=true&parent=streamhub.watch`;
+        }
         embedWrapper.style.display = "block";
-      }, 2000);
+      }, 3000);
     }));
     cards.forEach((card) => card.addEventListener("mouseleave", (event) => {
       const embedWrapper = card.querySelector(".embedWrapper");
-      const iframe = embedWrapper.querySelector("iframe")
-      iframe.src = ""
+      const iframe = embedWrapper.querySelector("iframe");
+      iframe.src = "";
       embedWrapper.style.display = "none";
     }));
   }
@@ -26,8 +29,6 @@ export { initEmbeddedStream }
 
 /*
 maybe dataset in card for building the iframe for retrieving the user
-select each card, 2 event listener: one time out for loading video, second one for change hidden class,
-on mouse leave set timeout + display="none" (maybe play with opacity, CSS animation)
 
 
 <iframe title="PaladinsGame's player frame" i18n-title="channel#ShareDialog:playerEmbedFrame|Embed player Frame copied from share dialog" allowfullscreen="true" src="https://mixer.com/embed/player/PaladinsGame?disableLowLatency=1" width="620" height="349"> </iframe>
